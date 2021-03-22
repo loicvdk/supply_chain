175001b6a620ad2393be5705f713c6b3f441541c
loicvdk <63651854+loicvdk@users.noreply.github.com>
2021-03-22 13:37:00 +0100

    added variables

--- main.py
+++ main.py
@@ -2,3 +2,37 @@
 ########## IMPORT ##########
 ############################
 import pulp as plp
+from materials import typeA, typeB, grade1, grade2, grade3
+from stages import stage1, stage2
+
+#################################
+########## MAIN SCRIPT ##########
+#################################
+
+T = 20  # days
+products = [typeA, typeB, grade1, grade2, grade3]
+stages = [stage1, stage2]
+
+
+if __name__ == "__main__":
+    # Define the variables
+    production = plp.LpVariable.dicts(
+        'production',
+        ((p, s, t) for p in products for s in stages for t in range(T)),
+        lowBound=0, cat='Continuous'
+    )
+
+    inventory = plp.LpVariable.dicts(
+        'inventory',
+        ((p, t) for p in products for t in range(T)),
+        lowBound=0, cat='Continuous'
+    )
+
+    is_produced = plp.LpVariable.dicts(
+        'is_p_produced',
+        ((p, s, t) for p in products for s in stages for t in range(T)),
+        cat=plp.LpBinary
+    )
+
+    production_costs = plp.LpVariable('production_cost', lowBound=0)
+    holding_costs = plp.LpVariable('holding_cost', lowBound=0)
